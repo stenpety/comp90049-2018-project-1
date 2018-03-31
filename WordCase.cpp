6c363480ddfe7a5080dafe6d8cc1907d27d06699
Petr Stenin <p.stenin@student.unimelb.edu.au>
2018-03-31 16:01:07 +1100

    Added methods for calculation of precision

--- WordCase.cpp
+++ WordCase.cpp
@@ -29,11 +29,13 @@ void WordCase::setMisspell_w(const std::string &misspell_w) {
     WordCase::misspell_w = misspell_w;
 }
 
-void WordCase::add_option(std::string add_w, gcnst::Methods method) {
+void WordCase::add_option(const std::string add_w, gcnst::Methods method) {
     switch (method) {
         case gcnst::GED:
             ged_opts->push_back(add_w);
             break;
+        case gcnst::LED:
+            break; // not implemented yet
         case gcnst::NGRAM:
             ngram_opts->push_back(add_w);
             break;
@@ -50,6 +52,8 @@ void WordCase::clear_options(gcnst::Methods method) {
         case gcnst::GED:
             ged_opts->clear();
             break;
+        case gcnst::LED:
+            break; // not implemented yet
         case gcnst::NGRAM:
             ngram_opts->clear();
             break;
@@ -60,3 +64,50 @@ void WordCase::clear_options(gcnst::Methods method) {
             break;
     }
 }
+
+double WordCase::update_precision(gcnst::Methods method) {
+    unsigned long words_sz = 0;
+    bool found = false;
+    double new_prec = 0;
+    switch (method) {
+        case gcnst::GED:
+            words_sz = ged_opts->size();
+            found = std::find(ged_opts->begin(), ged_opts->end(), correct_w) != ged_opts->end();
+            break;
+        case gcnst::LED:
+            break; // not implemented yet
+        case gcnst::NGRAM:
+            words_sz = ngram_opts->size();
+            found = std::find(ngram_opts->begin(), ngram_opts->end(), correct_w) != ngram_opts->end();
+            break;
+        case gcnst::SNDX:
+            words_sz = sndx_opts->size();
+            found = std::find(sndx_opts->begin(), sndx_opts->end(), correct_w) != sndx_opts->end();
+            break;
+        default:
+            break;
+    }
+
+    if (found) {
+        new_prec = 1.0 / words_sz;
+    }
+
+    return new_prec;
+}
+
+double WordCase::getGed_precision() {
+    ged_precision = update_precision(gcnst::GED);
+    return ged_precision;
+}
+
+double WordCase::getNgram_precision() {
+    ngram_precision = update_precision(gcnst::NGRAM);
+    return ngram_precision;
+}
+
+double WordCase::getSndx_precision() {
+    sndx_precision = update_precision(gcnst::SNDX);
+    return sndx_precision;
+}
+
+
